ec4eaa5e228f18422a2ade67db595bcbf6e11d1c
ConnerKT <connerthompson121@gmail.com>
2023-07-11 16:51:38 -0500

    solution almost

--- c-sharp/CodeChallenge3/ArrayBinarySearch.cs
+++ c-sharp/CodeChallenge3/ArrayBinarySearch.cs
@@ -4,8 +4,24 @@ namespace CodeChallenges
     {
         public static int BinarySearch(int[] array, int key)
         {
-            int foundElementIndex = -1;
-            return foundElementIndex;
+      int low = 0;
+      int mid = array.Length / 2;
+      int high = array.Length - 1;
+      if (key == array[mid])
+      {
+        return mid;
+      }
+      else if (key < array[mid])
+      {
+        high = mid - 1;
+        mid = (low + high) / 2;
+      }
+      else if (key > array[mid])
+      {
+        low = mid + 1;
+        mid = (low + high) / 2;
+      }
         }
+
     }
 }
