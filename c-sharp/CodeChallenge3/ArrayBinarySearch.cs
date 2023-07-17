650b628ce622ae5d01eb2733b2d557a1ac58896b
ConnerKT <connerthompson121@gmail.com>
2023-07-17 13:47:13 -0500

    challenge 3 commit

--- c-sharp/CodeChallenge3/ArrayBinarySearch.cs
+++ c-sharp/CodeChallenge3/ArrayBinarySearch.cs
@@ -1,27 +1,30 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class CodeChallenge3
+  public class CodeChallenge3
+  {
+    public static int BinarySearch(int[] array, int key)
     {
-        public static int BinarySearch(int[] array, int key)
-        {
       int low = 0;
-      int mid = array.Length / 2;
       int high = array.Length - 1;
-      if (key == array[mid])
-      {
-        return mid;
-      }
-      else if (key < array[mid])
-      {
-        high = mid - 1;
-        mid = (low + high) / 2;
-      }
-      else if (key > array[mid])
-      {
-        low = mid + 1;
-        mid = (low + high) / 2;
-      }
-        }
 
+      while (low <= high)
+      {
+        int mid = (low + high) / 2;
+
+        if (key == array[mid])
+        {
+          return mid;
+        }
+        else if (key < array[mid])
+        {
+          high = mid - 1;
+        }
+        else if (key > array[mid])
+        {
+          low = mid + 1;
+        }
+      }
+      return -1;
     }
+  }
 }
