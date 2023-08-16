9145347ce2a21ae4ed626cf3dd637ac9f6ba7760
ConnerKT <connerthompson121@gmail.com>
2023-08-15 22:20:06 -0500

    challenge 8 complete

--- c-sharp/CodeChallenge8/LinkedListZip.cs
+++ c-sharp/CodeChallenge8/LinkedListZip.cs
@@ -5,7 +5,30 @@ namespace CodeChallenges
   {
     public static LinkedListKth ZipLists(LinkedListKth list1, LinkedListKth list2)
     {
+      //the new list we want to return
       LinkedListKth result = new LinkedListKth();
+      //Setting the current nodes to each to our linked list
+      Node node1 = list1.Head;
+      Node node2 = list2.Head;
+      //While they are null, we will keep adding them to the list, null is the end
+      while(node1 != null || node2 != null)
+      {
+        //We want to make sure it isn't null
+        if (node1 != null)
+        {
+          //We add it to the list
+          result.Append(node1.Value);
+          //And then we go next
+          node1 = node1.Next;
+        }
+        if (node2 != null)
+        {
+          //We add it to the list
+          result.Append(node2.Value);
+          //And then we go next
+          node2 = node2.Next;
+        }
+      }
       return result;
     }
   }
